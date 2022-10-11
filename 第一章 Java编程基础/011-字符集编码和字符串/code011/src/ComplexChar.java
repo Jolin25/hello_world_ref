1c822a9e8593d1d0d52fe69c196baa4b8a31ab3c
Joly gm <j584189010@gmail.com>
2022-10-11 17:15:28 +0800

    utf-16 char

--- 第一章 Java编程基础/011-字符集编码和字符串/code011/src/ComplexChar.java
+++ 第一章 Java编程基础/011-字符集编码和字符串/code011/src/ComplexChar.java
@@ -1,12 +1,30 @@
+import java.io.UnsupportedEncodingException;
+
 public class ComplexChar {
-    public static void main(String[] args) {
-        int a = 65;
-        char cha = (char) a;
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        // int a = 65;
+        // char cha = (char) a;
+        // // \\u 是 Unicode
+        // char zang = '\u81e7';
+        //
+        // System.out.println(cha);
+        //
+        // System.out.println(zang);
 
-        char zang = '\u81e7';
+        new ComplexChar().jrl();
+    }
 
-        System.out.println(cha);
-
-        System.out.println(zang);
+    private void jrl() throws UnsupportedEncodingException {
+        byte[] bytes = "中".getBytes("utf-16");
+        for (byte aByte : bytes) {
+            System.out.println(Integer.toHexString(Byte.toUnsignedInt(aByte)));
+        }
+        byte[] bytes8 = "中".getBytes("utf-8");
+        for (byte aByte : bytes8) {
+            System.out.println(Integer.toHexString(Byte.toUnsignedInt(aByte)));
+        }
+        String s = "ﾖ"; //0xFF6E
+        System.out.println(s.getBytes().length);//length of the string
+        System.out.println(s.charAt(0));//first character in the string
     }
 }
