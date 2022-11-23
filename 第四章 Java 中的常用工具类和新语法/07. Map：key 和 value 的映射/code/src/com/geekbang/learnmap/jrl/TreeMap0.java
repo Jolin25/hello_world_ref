15e481f0bbdc7afe6406c21e447f409b8787b420
Joly163 <18990341730@163.com>
2022-11-23 14:49:17 +0800

    .

---
+++ 第四章 Java 中的常用工具类和新语法/07. Map：key 和 value 的映射/code/src/com/geekbang/learnmap/jrl/TreeMap0.java
@@ -0,0 +1,13 @@
+package com.geekbang.learnmap.jrl;
+
+import java.util.TreeMap;
+
+/**
+ * @author jrl
+ * @date Create in 22:48 2022/11/22
+ */
+public class TreeMap0 {
+    public static void main(String[] args) {
+        TreeMap<String, String> treeMap = new TreeMap<>();
+    }
+}
